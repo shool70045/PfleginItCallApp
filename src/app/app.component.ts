81721cbdb79f445a5f557d4e6f02ddbdf43e937a
shoolpani dubey <shool.pani.dubey@gmail.com>
2020-03-23 00:12:59 +0530

    Added support to load users camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, AbstractType } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -6,11 +6,66 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = "PfleginItCallApp";
-  vedioSrc = [
-    {
-      value: "0",
-      text: "Video Src 1"
-    }
-  ];
+  title: string = "PfleginItCallApp";
+  vedioSrcList: any = [];
+  constraints = {
+    audio: false,
+    video: true
+  };
+  vdoSrc: MediaStream;
+  selectedVdoSrc: any;
+
+  constructor() {
+    // Get the initial set of cameras connected
+    // this.vedioSrcList = this.getConnectedDevices("videoinput");
+    this.getConnectedDevices();
+    //this.registerEventListener();
+  }
+
+  // Fetch an array of devices of a certain type
+  getConnectedDevices = () => {
+    let _this = this;
+    // const devices = await navigator.mediaDevices.enumerateDevices();
+    // return devices.filter(device => device.kind === type);
+
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(this.setConnectedDevices)
+      .catch(this.handleError);
+  };
+
+  localVedioDevice = (videoSource: any) => {
+    let _this = this;
+    const constraints = {
+      video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function(stream: MediaStream) {
+        _this.vdoSrc = stream;
+      })
+      .catch(function(e) {
+        console.log("Error:", e);
+      });
+  };
+
+  setConnectedDevices = devices => {
+    this.vedioSrcList = devices.filter(device => device.kind === "videoinput");
+  };
+
+  handleError = e => {
+    console.error("Error:", e);
+  };
+
+  registerEventListener = () => {
+    // Listen for changes to media devices and update the list accordingly
+    navigator.mediaDevices.addEventListener("devicechange", event => {
+      this.getConnectedDevices();
+    });
+  };
+
+  onVedioSelect = () => {
+    // console.log("Sele:", this.selectedVdoSrc);
+    this.localVedioDevice(this.selectedVdoSrc);
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -13,6 +13,7 @@ import { MatIconModule } from "@angular/material/icon";
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
